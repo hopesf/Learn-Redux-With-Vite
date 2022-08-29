12ca512b8ccd09a2535bd1931de524caaf8f64d4
Selim Geçin <gecin18@gmail.com>
2022-08-29 17:14:02 +0300

    createStore

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createStore } from "redux";
+
+function reducer() {
+  return "state";
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
